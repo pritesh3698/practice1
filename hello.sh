c5cd5f922df4c0b930ca689dedd441abf0af582a
pritesh.mahajan <pritesh.mahajan@infosys.com>
2022-06-17 16:38:24 +0530

    commited

---
+++ hello.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+git init
+git checkout -b pm
+git add .
+git commit -m "commited"
+
+git remote add $i https://infygithub.ad.infosys.com/pritesh-mahajan/practice3.git
+  git push -u $ipm
+
+
+
